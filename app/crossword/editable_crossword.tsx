863ab806374dced8954854793b1906f34b2c8e26
Jessie Douglas <jessielou.douglas@gmail.com>
2025-04-02 15:54:52 -0600

    Basic grid with input squares

---
+++ app/crossword/editable_crossword.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import './crossword.css';
+
+const SQUARE_PIXELS = 24;
+
+function EditableSquare() {
+    return (
+        <div className="crossword-square size-[24px] border-2 border-black border-b-0 box-content">
+            <input className="crossword-input size-full"></input>
+        </div>
+    );
+}
+
+export default function EditableCrossword() {
+    const NUM_ROWS = 15;
+    const NUM_COLUMNS = 15;
+    const numSquares = NUM_ROWS * NUM_COLUMNS;
+    const squares = []
+    for (let i=0; i<numSquares; i++) {
+        squares.push(<EditableSquare key={`square-${i}`}></EditableSquare>)
+    }
+    const style = {width: `${NUM_COLUMNS * 24}px`} // TODO: make 24 a constant
+    return (
+        <div className={`crossword grid grid-cols-${NUM_COLUMNS} grid-flow-row  border-b-2 border-black box-content`} style={style}>
+            {squares}
+        </div>
+    );
+}
