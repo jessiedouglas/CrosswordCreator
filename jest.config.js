76982c98ebf01cd01d11bd3574fb041ebda2eadd
jessiedouglas <jessielou.douglas@gmail.com>
2025-04-14 21:26:22 -0600

    Set up jest preview

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,28 @@
 const config = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
   moduleNameMapper: {
-    '^.+\\.(css|less)$': '<rootDir>/testing/CSSStub.js'
+    '^.+\\.(css|less)$': '<rootDir>/testing/CSSStub.js',
+    // Handle module aliases
+    '^@/components/(.*)$': '<rootDir>/components/$1',
   },
-  testEnvironment: 'jsdom'
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(css|scss|sass|less)$': 'jest-preview/transforms/css',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': 'jest-preview/transforms/file',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
 };
 
 module.exports = config;
---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+import { jestPreviewConfigure } from 'jest-preview';
+import 'app/globals.css';
+import 'app/crossword/crossword.css';
+
+jestPreviewConfigure({ autoPreview: true });
